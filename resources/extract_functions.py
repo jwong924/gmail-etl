f35a3357f1c140970d406dbaefa30bac5cc25f5e
jwong924 <jonwong52@gmail.com>
2022-12-20 21:44:35 -0600

    update

--- resources/extract_functions.py
+++ resources/extract_functions.py
@@ -2,7 +2,7 @@ import requests
 import json
 import base64 as base64
 from bs4 import BeautifulSoup as BeautifulSoup
-from resources.get_token import get_token
+from resources.get_creds import get_creds
 
 # Query into completed pages
 def find_item(key,value,content):
@@ -15,10 +15,7 @@ def find_item(key,value,content):
   
 # Get a list of emails by page
 def get_list(query):
-    try:
-        with open('token.json') as file:
-            token = json.load(file)['token']
-        headers = {'Authorization':'Bearer '+token}
+    creds = get_creds()
     except:
         token = json.loads(get_token())['token']
     r = requests.get('https://gmail.googleapis.com/gmail/v1/users/me/messages'+query, headers=headers)
