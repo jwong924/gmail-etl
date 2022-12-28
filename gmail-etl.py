010b75d57d198f5201286e325a1f21194592a8a5
jwong924 <jonwong52@gmail.com>
2022-12-28 11:45:29 -0600

    Update gmail-etl.py

--- gmail-etl.py
+++ gmail-etl.py
@@ -75,7 +75,7 @@ def extract():
             print('********** next page token: '+str(nextPageToken)+' **********')
         conn.commit()
     except Exception as e:
-        print('Extract Function Error: '+str(e))
+        print('Extract Function Error: '+str(e)+ ' at line: '+str(sys.exc_info().tb_lineno))
     finally:
         conn.close()
     print('Queried: '+str(len(msgs))+' Emails')
