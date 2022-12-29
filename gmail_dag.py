92738141341c86e4d17ee144151b3c95975e00b1
jwong924 <jonwong52@gmail.com>
2022-12-29 09:46:40 -0600

    Update gmail_dag.py

--- gmail_dag.py
+++ gmail_dag.py
@@ -1,11 +1,9 @@
 from resources.gmail_functions import extract, transform_load_raw
-import datetime
+from datetime import datetime
 from airflow.models.dag import DAG
 
-with DAG('gmail_etl_dag', schedule_interval='@daily', start_date=datetime(2022,12,1),catchup=False) as dag:
-    
+with DAG("gmail_etl_dag", schedule_interval='@daily', start_date=datetime(2022,12,1), catchup=False) as dag:
     raw_emails = extract()
-
-    format_emails = transform_load_raw
+    format_emails = transform_load_raw()
 
     raw_emails >> format_emails
