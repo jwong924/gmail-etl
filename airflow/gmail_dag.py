5c631cb24387ca25e8c8e8ab650de6b9bbdb95f4
jwong924 <jonwong52@gmail.com>
2022-12-28 22:10:12 -0600

    change folder structure

--- airflow/gmail_dag.py
+++ airflow/gmail_dag.py
@@ -1,4 +1,4 @@
-import etl_gmail
+import resources as gmail_functions
 import datetime
 from airflow.models.dag import DAG
 from airflow.decorators import task
@@ -8,14 +8,14 @@ from airflow.utils.task_group import TaskGroup
 
 with DAG(dag_id='gmail_etl_dag', schedule='@daily', start_date=datetime(2022,12,1),catchup=False) as dag:
     with TaskGroup("extract_gmail_write", tooltip="Extract raw gmail data") as extract_load_raw:
-        raw_emails = etl_gmail.extract()
-        load_raw_emails = etl_gmail.write_raw(raw_emails)
+        raw_emails = gmail_functions.extract()
+        load_raw_emails = gmail_functions.write_raw(raw_emails)
 
         raw_emails >> load_raw_emails
     
     with TaskGroup("transform_raw") as format_load_raw:
-        format_emails = etl_gmail.transform_raw(raw_emails)
-        load_format_emails = etl_gmail.write_stage_1(format_emails)
+        format_emails = gmail_functions.transform_raw(raw_emails)
+        load_format_emails = gmail_functions.write_stage_1(format_emails)
 
         format_emails >> load_format_emails
 
---
+++ airflow/resources/__init__.py
@@ -0,0 +1,1 @@
+from resources.gmail_functions import *
